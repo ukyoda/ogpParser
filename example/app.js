f6a7f60d83fd02d2f75a31f55840c8505046f33e
ukyoda <black12172001@hotmail.co.jp>
2015-04-01 01:17:39 +0900

    サンプル修正
    URLを固定で設定していたところをコマンドライン引数で指定できるように
    変更

--- example/app.js
+++ example/app.js
@@ -1,5 +1,12 @@
+var url = process.argv[2] || null;
+
+if(!url || url.match(/^(http|https)\:\/\//g) === null) {
+    console.log("Usage: ");
+    console.log("node app.js [url]");
+    return;
+}
+
 var ogp = require("../ogpParser");
-var url = "http://ogp.me";
 console.log("URL:"+url);
 ogp.parser(url,function(error,data){
 	console.log(data);
