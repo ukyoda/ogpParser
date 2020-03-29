ab43e98ee55385d54b060b9b397dc38a4b11196c
ukyoda <black12172001@hotmail.co.jp>
2020-03-29 13:16:51 +0900

    HTTPリクエスト方法をnode.jsの標準APIからaxiosに変更
    * redirectFlgをDepricationに変更 (実質無効化)

--- test/request.js
+++ test/request.js
@@ -2,51 +2,39 @@ var chai = require('chai');
 var assert = chai.assert;
 var parser = require('../ogpParser');
 
-function resCheck(it, data) {
-  it('指定したキーが全て存在する', () => {
-    assert.containsAllKeys(data, ['title', 'ogp', 'seo']);
-  });
-
-  it('プロパティ`title`は文字列', () => {
-    assert.isString(data.title);
-  });
-
-  it('プロパティ`ogp`はオブジェクト', () => {
-    assert.isObject(data.ogp);
-  });
-
-  it('プロパティ`seo`はオブジェクト', () => {
-    assert.isObject(data.seo);
-  });
+function resCheck(data) {
+  assert.containsAllKeys(data, ['title', 'ogp', 'seo'], '指定したキーが全て存在する');
+  assert.isString(data.title, 'プロパティ`title`は文字列');
+  assert.isObject(data.ogp, 'プロパティ`ogp`はオブジェクト');
+  assert.isObject(data.seo, 'プロパティ`seo`はオブジェクト');
 }
 
-describe('正常リクエスト：HTTPリクエストが正常に処理される', function () {
-  this.timeout(5000);
-  parser('http://example.com', false).then(data => {
-    resCheck(it, data);
-  });
-});
-
-describe('正常リクエスト：SSLリクエストが正常に処理される', function() {
-  this.timeout(5000);
-  parser('https://www.example.com', true).then(data => {
-    resCheck(it, data);
-  });
-});
-
-function failCheck(url) {
-  return new Promise((resolve, reject) => {
-    parser(url).then(data => {
-      reject(`resolveが返却されている(url=${url})`);
-    }).catch((err) => {
-      resolve('OK');
+describe('HTTPリクエストテスト', function () {
+  it ('正常リクエスト: HTTPリクエストが正常に処理される', function (done) {
+    parser('http://example.com', false).then(data => {
+      resCheck(data);
+      done();
+    }).catch(err => {
+      assert.fail(err);
+      done();
     });
   });
-}
-
-describe('異常系：処理確認', function() {
-  //this.timeout(5000);
-  it('存在しないURLにアクセス', function () {
-    return failCheck('http://abc.example.com/')
+  it('正常リクエスト：SSLリクエストが正常に処理される', function (done) {
+    parser('https://www.example.com', true).then(data => {
+      resCheck(data);
+      done();
+    }).catch(err => {
+      assert.fail(err);
+      done();
+    });
+  });
+  it('異常系: 存在しないURLにアクセス', function (done) {
+    parser('http://abc.example.com').then(data => {
+      assert.fail(`resolveが返却されている(url=${url})`)
+      done();
+    }).catch(err => {
+      assert.ok('OK', `Catch OK(err=${err})`);
+      done();
+    })
   });
 });
