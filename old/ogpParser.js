5ac5e669be78e252a18bef58f6bc009e392aeffc
ukyoda <black12172001@hotmail.co.jp>
2020-04-01 23:27:50 +0900

    oEmbedUrl情報が残ったままだった

--- old/ogpParser.js
+++ old/ogpParser.js
@@ -6,6 +6,7 @@ console.warn(`                       Please Use future version \`const parser = 
 var parser = function (url, redirectFlg) {
   return getContents(url).then(function (html) {
     let data = parseHtml(html);
+    delete data.oembedInfo;
     return Promise.resolve(data);
   });
 };
