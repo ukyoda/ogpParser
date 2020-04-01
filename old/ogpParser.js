34c8416ce65f4bedaf076bdf828b574ddd3e1e76
ukyoda <black12172001@hotmail.co.jp>
2020-04-01 23:32:18 +0900

    「Deprication Warning」の文言修正

--- old/ogpParser.js
+++ old/ogpParser.js
@@ -1,6 +1,6 @@
 var getContents = require('../utils/getContents');
 var parseHtml = require('../utils/parseHtml');
-console.warn('[Deprication Warning]: RedirectFlg is disabled. This Variable is remove at future version.');
+console.warn('[Deprication Warning]: This API is remove at future version.');
 console.warn(`                       Please Use future version \`const parser = require('ogp-parser');\`\n\n`);
 
 var parser = function (url, redirectFlg) {
