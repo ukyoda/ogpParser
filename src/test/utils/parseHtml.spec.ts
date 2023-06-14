f3caf1193a5405215e8f5b5ea95410017ec57535
ukyoda <black12172001@hotmail.co.jp>
2023-06-14 17:21:44 +0900

    fix format

--- src/test/utils/parseHtml.spec.ts
+++ src/test/utils/parseHtml.spec.ts
@@ -1,38 +1,47 @@
-import { parseHtml } from "../../utils/parseHtml";
-import fs from 'fs'
-import path from 'path'
+import { parseHtml } from '../../utils/parseHtml';
+import fs from 'fs';
+import path from 'path';
 
-const html = fs.readFileSync(path.join(__dirname, '../fixture/demo.html'), 'utf-8')
-const htmlOembed = fs.readFileSync(path.join(__dirname, '../fixture/demo_oembed.html'), 'utf-8')
-const htmlOembedXml = fs.readFileSync(path.join(__dirname, '../fixture/demo_oembed_xml.html'), 'utf-8')
+const html = fs.readFileSync(
+  path.join(__dirname, '../fixture/demo.html'),
+  'utf-8'
+);
+const htmlOembed = fs.readFileSync(
+  path.join(__dirname, '../fixture/demo_oembed.html'),
+  'utf-8'
+);
+const htmlOembedXml = fs.readFileSync(
+  path.join(__dirname, '../fixture/demo_oembed_xml.html'),
+  'utf-8'
+);
 
 describe('parseHtml test', () => {
-  it ('ogp values check', () => {
-    const { ogp } = parseHtml(html)
-    expect(ogp['og:title'][0]).toBe('このHTMLはテスト用です。')
-    expect(ogp['og:type'][0]).toBe('website')
-    expect(ogp['og:url'][0]).toBe('http://example.com')
-    expect(ogp['og:image'].length).toBe(2)
-    expect(ogp['og:image'][0]).toBe('http://hoge.example.com/test1.jpg')
-    expect(ogp['og:image'][1]).toBe('http://hoge.example.com/test2.jpg')
-    expect(ogp['fb:admins'][0]).toBe('XXXXXXXXXX')
-    expect(ogp['fb:app_id'][0]).toBe('YYYYYYYYYY')
-  })
+  it('ogp values check', () => {
+    const { ogp } = parseHtml(html);
+    expect(ogp['og:title'][0]).toBe('このHTMLはテスト用です。');
+    expect(ogp['og:type'][0]).toBe('website');
+    expect(ogp['og:url'][0]).toBe('http://example.com');
+    expect(ogp['og:image'].length).toBe(2);
+    expect(ogp['og:image'][0]).toBe('http://hoge.example.com/test1.jpg');
+    expect(ogp['og:image'][1]).toBe('http://hoge.example.com/test2.jpg');
+    expect(ogp['fb:admins'][0]).toBe('XXXXXXXXXX');
+    expect(ogp['fb:app_id'][0]).toBe('YYYYYYYYYY');
+  });
 
-  it ('seo values check', () => {
-    const { seo } = parseHtml(html)
-    expect(seo['viewport'][0]).toBe('width=device-width,initial-scale=1')
-    expect(seo['description'][0]).toBe('テストDescription')
-  })
+  it('seo values check', () => {
+    const { seo } = parseHtml(html);
+    expect(seo['viewport'][0]).toBe('width=device-width,initial-scale=1');
+    expect(seo['description'][0]).toBe('テストDescription');
+  });
 
-  it ('oembed tag check', () => {
-    const { oembedInfo: oembedMiss } = parseHtml(html)
-    const { oembedInfo } = parseHtml(htmlOembed)
-    const { oembedInfo: oembedInfoXML } = parseHtml(htmlOembedXml)
-    expect(oembedMiss).toBeUndefined()
-    expect(oembedInfo?.type).toBe('json')
-    expect(oembedInfo?.url).toBe('https://oembed.example.com/jsondata')
-    expect(oembedInfoXML?.type).toBe('xml')
-    expect(oembedInfoXML?.url).toBe('https://oembed.example.com/xmldata')
-  })
-})
+  it('oembed tag check', () => {
+    const { oembedInfo: oembedMiss } = parseHtml(html);
+    const { oembedInfo } = parseHtml(htmlOembed);
+    const { oembedInfo: oembedInfoXML } = parseHtml(htmlOembedXml);
+    expect(oembedMiss).toBeUndefined();
+    expect(oembedInfo?.type).toBe('json');
+    expect(oembedInfo?.url).toBe('https://oembed.example.com/jsondata');
+    expect(oembedInfoXML?.type).toBe('xml');
+    expect(oembedInfoXML?.url).toBe('https://oembed.example.com/xmldata');
+  });
+});
